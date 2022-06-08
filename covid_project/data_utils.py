86d7bef41899bd6c411dc146bc0b92e9d503c05d
Alex Palermo <alex.palermo@armadaiq.com>
2022-06-08 18:06:46 -0500

    started moving correlation analysis to notebook

--- covid_project/data_utils.py
+++ covid_project/data_utils.py
@@ -9,8 +9,10 @@ from sodapy import Socrata
 from tqdm import tqdm
 import numpy as np
 from datetime import timedelta
+from datetime import datetime
 import us
 import re
+from tqdm import tqdm
 
 ##################################################################################
 ### DATA INGESTION ###############################################################
@@ -489,3 +491,123 @@ def get_policies(policy_dataframe=None,
     # county policies only
     else:  
          return policy_dataframe[ (policy_dataframe['state'] == state) & (policy_dataframe["county"] == county)]
+
+def generate_state_case_dict(case_df,
+                             save_data=False,
+                             load_from_file=False,
+                             path="./data/state_data/"):
+    state_cases_dict = dict()
+    for state in tqdm([elem.name for elem in us.states.STATES]): 
+        state_save_path = path+str(state)+".csv"
+
+        if load_from_file and os.path.exists(state_save_path):
+            state_data = pd.read_csv(state_save_path, index_col=0)
+        else:
+            state_data = get_cases(case_dataframe=case_df,
+                                            level="state",
+                                            state=state);
+            if save_data:
+                if not os.path.exists(path):
+                    os.makedirs(path)
+                state_data.to_csv(state_save_path)
+        state_cases_dict[state] = state_data
+    return state_cases_dict
+
+def calculate_deltas(case_df,
+                     policy_df,
+                     measure_period=14,
+                     filtered_policies=None,
+                     state_cases_dict=None,
+                     save_state_data=True,
+                     load_state_data_from_file=True,
+                     state_data_path="./data/state_data/"): 
+    """For every policy implementation at the state and county level, calculate the change in case and death numbers. 
+    
+    Parameters
+    ---------- 
+    measure_period : int
+        time to wait (in days) before measuring a change in new case or death numbers (14 by default)
+    filtered_policies : array-like 
+        specify policies to select (defaul: None- calulate deltas for all policies)
+    case_df : pandas DataFrame
+        DataFrame with case / death information (default: df)
+    policy_df : pandas DataFrame
+        DataFrame with police information (default: df2)
+    
+    Returns
+    ----------  
+    A copy of the covid policies df (df2) with 2 appended columns for the change in case and death numbers. 
+    """
+    
+    # Initialize wait period before measurement.
+    wait_period = timedelta(days=measure_period)
+    day_1 = timedelta(days=1)
+    
+    def sub_calc_deltas(ser, date, wait=wait_period): 
+        """Wrap repeated calculations in a sub function to avoid repetition."""
+        day_1 = timedelta(days=1)
+
+        print("series = ", ser)
+        # ser.index = datetime.strptime(ser.index, "%Y-%m-%d")
+    
+        start      = ser[ser.index==date].values[0]
+        start_1day = ser[ser.index==date+day_1].values[0]
+        
+        end        = ser[ser.index==date+wait].values[0]
+        end_1day   = ser[ser.index==date+wait+day_1].values[0]
+        
+        return [start, start_1day, end, end_1day]
+    
+    # If there we are only examining select policies, then filter those out.
+    if filtered_policies is not None: 
+        policy_df = policy_df.loc[policy_df['policy_type'].isin(filtered_policies)]
+        
+    correlated_df = policy_df.copy()
+    
+    # Initially fill the delta column with nan.
+    correlated_df.loc[:, f"case_{measure_period}_day_delta"] = np.nan
+    correlated_df.loc[:, f"case_{measure_period}_day_accel"] = np.nan
+    correlated_df.loc[:, f"death_{measure_period}_day_delta"] = np.nan
+    correlated_df.loc[:, f"death_{measure_period}_day_accel"] = np.nan
+    
+    # Load all state-aggregated datasets into a dictionary. We expect to need all 50 states so let's take the time to aggregate
+    # the state data now so we don't need to do it repeatedly in the loop. 
+    
+    if state_cases_dict is None: 
+        state_cases_dict = generate_state_case_dict(case_df=case_df,
+                                                    save_data = save_state_data,
+                                                    load_from_file = load_state_data_from_file,
+                                                    path = state_data_path)
+
+    case_df = case_df.set_index('date')
+    total_policies = len(policy_df)
+    
+    for index, data in tqdm(policy_df.iterrows()): 
+        data['date'] = datetime.strptime(data['date'], "%Y-%m-%d")
+        # If this is a state-level policy, then we already have the DataFrame to use. 
+        if data.policy_level == 'state': 
+            state_df = state_cases_dict[data.state]
+            ser_cases = state_df['new_cases_7day_1e6' ]
+            ser_deaths = state_df['new_deaths_7day_1e6']
+        
+        # This must be a county level policy- filter the appropriate data. 
+        else:
+            ser_cases = case_df['new_cases_7day_1e6' ][case_df['fips_code'] == data.fips_code]
+            ser_deaths = case_df['new_deaths_7day_1e6'][case_df['fips_code'] == data.fips_code]
+        
+        # Get the case and death numbers at the appropriate days. 
+        c11, c12, c21, c22 = sub_calc_deltas(ser_cases, date=data.date)
+        d11, d12, d21, d22 = sub_calc_deltas(ser_deaths, date=data.date)
+           
+        # Calculate the difference in new cases at the selected dates. 
+        correlated_df.at[index, f"case_{measure_period}_day_delta"] = c21 - c11
+        correlated_df.at[index, f"death_{measure_period}_day_delta"] = d21 - d11
+        
+        # Calculate the change in curvature (aka acceleration) of the case / death plots at policy implementation and
+        # measure_period days afterwards. 
+        
+        correlated_df.at[index, f"case_{measure_period}_day_accel"] = ((c12-c11) - (c21-c22)) / measure_period
+        correlated_df.at[index, f"death_{measure_period}_day_accel"] = ((d12-d11) - (d21-d22)) / measure_period    
+    
+    
+    return correlated_df, state_cases_dict
